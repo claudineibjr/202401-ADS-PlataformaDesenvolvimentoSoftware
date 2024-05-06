291eeb23723dcae795baa308450a28eee7478008
ClaudineiBJr <claudineibjr@hotmail.com>
2024-05-06 08:23:46 -0300

    [Aula16APIFilmes] Implementação dos métodos HTTP

--- Aula16APIFilmes/Program.cs
+++ Aula16APIFilmes/Program.cs
@@ -1,7 +1,8 @@
 namespace Aula16APIFilmes
 {
-    class Filme
+    public class Filme
     {
+        public int Id { get; set; }
         public string Titulo { get; set; }
         public int AnoLancamento { get; set; }
         public double NotaIMDB { get; set; }
@@ -16,6 +17,9 @@ namespace Aula16APIFilmes
 
     public class Program
     {
+
+        public static List<Filme> filmes = new List<Filme>();
+
         public static void Main(string[] args)
         {
             // Criação da WebApplication
@@ -37,19 +41,81 @@ namespace Aula16APIFilmes
 
             #region Endpoints
             // GET      /filmes
-            app.MapGet("/filmes", () => { /* IMPLEMENTAÇÃO */ });
+            app.MapGet("/filmes", () =>
+            {
+                // Retorna todos os filmes
+                return Results.Ok(filmes);
+            });
 
             // GET      /filmes/{Id}
-            app.MapGet("/filmes/{Id}", (int Id) => { /* IMPLEMENTAÇÃO */ });
+            app.MapGet("/filmes/{Id}", (int Id) =>
+            {
+                // Procura pelo filme com o Id recebido
+                Filme? filme = filmes.Find(u => u.Id == Id);
+                if (filme is null)
+                {
+                    // Indica que o filme não foi encontrado
+                    return Results.NotFound();
+                }
+
+                // Devolve o filme encontrado
+                return Results.Ok(filme);
+            });
 
             // POST     /filmes
-            app.MapPost("/filmes", (Filme filme) => { /* IMPLEMENTAÇÃO */ });
+            app.MapPost("/filmes", (Filme filme) =>
+            {
+                if (filmes.Count() == 0)
+                {
+                    // Atribui o Id 1 ao novo filme já que não tem nenhum filme cadastrado
+                    filme.Id = 1;
+                }
+                else
+                {
+                    // Atribui o Id ao novo filme como o Id máximo + 1
+                    filme.Id = 1 + filmes.Max(u => u.Id);
+                }
+                filmes.Add(filme);
+
+                return Results.Created($"/filmes/{filme.Id}", filme);
+            });
 
             // PUT      /filmes/{Id}
-            app.MapPut("/filmes/{Id}", (int id, Filme filme) => { /* IMPLEMENTAÇÃO */ });
+            app.MapPut("/filmes/{Id}", (int Id, Filme filme) =>
+            {
+                // Encontra o filme especificado buscando pelo Id enviado
+                int indiceFilme = filmes.FindIndex(u => u.Id == Id);
+                if (indiceFilme == -1)
+                {
+                    // Indica que o filme não foi encontrado
+                    return Results.NotFound();
+                }
+
+                // Mantém o Id do filme como o Id existente
+                filme.Id = Id;
+
+                // Atribui o filme enviado na lista de filmes
+                filmes[indiceFilme] = filme;
+
+                return Results.NoContent();
+            });
 
             // DELETE   /filmes/{Id}
-            app.MapDelete("/filmes/{Id}", (int id) => { /* IMPLEMENTAÇÃO */ });
+            app.MapDelete("/filmes/{Id}", (int Id) =>
+            {
+                // Encontra o filme especificado buscando pelo Id enviado
+                int indiceFilme = filmes.FindIndex(u => u.Id == Id);
+                if (indiceFilme == -1)
+                {
+                    // Indica que o filme não foi encontrado
+                    return Results.NotFound();
+                }
+
+                // Remove o filme encontrado da lista de filmes
+                filmes.RemoveAt(indiceFilme);
+
+                return Results.NoContent();
+            });
             #endregion
 
             // Execução da aplicação
