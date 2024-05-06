7d99c64c68f3fd1ee199d8229a14a3d445befcd3
ClaudineiBJr <claudineibjr@hotmail.com>
2024-05-06 08:07:40 -0300

    [Aula16APIFilmes] Inicialização do Swagger

--- Aula16APIFilmes/Program.cs
+++ Aula16APIFilmes/Program.cs
@@ -7,8 +7,19 @@ namespace Aula16APIFilmes
             // Criação da WebApplication
             var builder = WebApplication.CreateBuilder(args);
 
+            // Configuração do Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             // Construção da WebApplication
             var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                // Inicialização do Swagger
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             #region Endpoints
             
