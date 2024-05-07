99035863efdbdef634d64959143f5064d0f3a098
ClaudineiBJr <claudineibjr@hotmail.com>
2024-05-07 09:51:31 -0300

    [Aula16APIFilmes] [BancoDados] Adaptação da classe de modelo Filme

--- Aula16APIFilmes/Models/Filme.cs
+++ Aula16APIFilmes/Models/Filme.cs
@@ -1,11 +1,18 @@
-﻿namespace Aula16APIFilmes.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aula16APIFilmes.Models
 {
     public class Filme
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
         public int Id { get; set; }
         public string Titulo { get; set; }
         public int AnoLancamento { get; set; }
         public double NotaIMDB { get; set; }
+
+        private Filme() { }
 
         public Filme(string titulo, int anoLancamento, double notaIMDB)
         {
