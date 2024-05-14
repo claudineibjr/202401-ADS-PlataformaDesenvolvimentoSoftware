b0c0d93bf2028d8df089d89e908218d39ae0d189
ClaudineiBJr <claudineibjr@hotmail.com>
2024-05-14 20:42:41 -0300

    [APIFilmes] Criaçao do projeto

---
+++ APIArquivos/Program.cs
@@ -0,0 +1,30 @@
+namespace APIArquivos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Criação da WebApplication
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Configuração do Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Construção da WebApplication
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                // Inicialização do Swagger
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.MapGet("/", () => "Hello World");
+
+            // Execução da aplicação
+            app.Run();
+        }
+    }
+}
