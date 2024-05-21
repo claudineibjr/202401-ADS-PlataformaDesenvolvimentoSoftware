97eb2782aa8c222f9f21ed259b1b8e64901450f4
ClaudineiBJr <claudineibjr@hotmail.com>
2024-05-21 17:44:54 -0300

    [Aula16APIFilmes] Nova classe Usuario

---
+++ Aula16APIFilmes/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿namespace Aula16APIFilmes.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+        public Usuario(string nome, string email, string senha)
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+        }
+    }
+}
