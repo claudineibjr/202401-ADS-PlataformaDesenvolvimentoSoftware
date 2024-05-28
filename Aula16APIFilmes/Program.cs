818370d51de61efb9f808663d5fd9c6da8068836
ClaudineiBJr <claudineibjr@hotmail.com>
2024-05-28 09:15:59 -0300

    [Aula16APIFilmes] Configuração de middleware e variáveis de ambiente para geração de token JWT (2)

--- Aula16APIFilmes/Program.cs
+++ Aula16APIFilmes/Program.cs
@@ -1,6 +1,9 @@
 using Aula16APIFilmes.Database;
 using Aula16APIFilmes.Endpoints;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Aula16APIFilmes
 {
@@ -24,6 +27,21 @@ namespace Aula16APIFilmes
                         builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
                     });
             });
+
+            // Configuração de autenticação e autorização
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SECRET_KEY"]!))
+                    };
+                });
+            builder.Services.AddAuthorization();
 
             // Configuração do Banco de Dados
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
