7ee8eddf6de957813b3615b3a0cf12601f8c8c24
ClaudineiBJr <claudineibjr@hotmail.com>
2024-05-28 09:33:26 -0300

    [Aula16APIFilmes] Implementação de novo endpoint /auth/me

--- Aula16APIFilmes/Endpoints/AuthEndpoints.cs
+++ Aula16APIFilmes/Endpoints/AuthEndpoints.cs
@@ -1,6 +1,8 @@
 ﻿using Aula16APIFilmes.Database;
 using Aula16APIFilmes.Models;
 using Aula16APIFilmes.Utils;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Aula16APIFilmes.Endpoints
 {
@@ -15,6 +17,22 @@ namespace Aula16APIFilmes.Endpoints
         public static void RegistrarEndpointsAutenticacao(this IEndpointRouteBuilder rotas)
         {
             RouteGroupBuilder rotasAuth = rotas.MapGroup("/auth");
+
+            rotasAuth.MapGet("/me", (MeusFilmesDbContext dbContext, ClaimsPrincipal usuarioLogado) => {
+                var usuarioLogadoId = usuarioLogado.Claims.FirstOrDefault(c => c.Type == "id");
+                if (usuarioLogadoId is null)
+                {
+                    return Results.NotFound();
+                }
+
+                Usuario? usuarioEncontrado = dbContext.Usuarios.FirstOrDefault(u => u.Id.ToString() == usuarioLogadoId.Value.ToString());
+                if (usuarioEncontrado == null)
+                {
+                    return Results.NotFound();
+                }
+
+                return Results.Ok(usuarioEncontrado.GetUsuarioDtoOutput());
+            }).RequireAuthorization();
 
             rotasAuth.MapPost("/signIn", (MeusFilmesDbContext dbContext, ITokenService tokenService, SignInParameters usuario) => {
                 Usuario? usuarioEncontrado = dbContext.Usuarios.FirstOrDefault(u => u.Email == usuario.Email && u.Senha == usuario.Senha);
